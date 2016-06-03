83e3c89ab568325841893d36fd7b335aadd70323
jony <343851557@qq.com>
2016-06-03 18:06:00 +0800

    Junit MockMvc test

--- src/main/java/com/song/nail/controller/HelloWorldController.java
+++ src/main/java/com/song/nail/controller/HelloWorldController.java
@@ -4,24 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.song.nail.entity.Account;
 import com.song.nail.service.AccountService;
 
 @Controller
-@RequestMapping(value="/hello")
 public class HelloWorldController {
 	@Autowired
 	private AccountService accountService;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String hello(Model model){
+	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	public ModelAndView hello(){
 		List<Account> accounts = accountService.getAccountList();
-		model.addAttribute("accounts", accounts);
-		return "hello";
+		ModelAndView mav = new ModelAndView();
+        mav.setViewName("hello");
+        mav.addObject("accounts", accounts);
+        return mav;
 	}
 
 	public void setAccountService(AccountService accountService) {
